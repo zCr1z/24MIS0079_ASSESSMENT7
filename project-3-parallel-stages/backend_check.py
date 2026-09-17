daa184d93f578828f73ae14efbfcb39d03972527
Chris Thomas Binoj <91975002+zCr1z@users.noreply.github.com>
2026-09-17 04:57:22 +0000

    3rd Project

---
+++ project-3-parallel-stages/backend_check.py
@@ -0,0 +1,5 @@
+import time
+
+print("Running backend checks...")
+time.sleep(3)
+print("Backend checks passed.")
---
+++ project-3-parallel-stages/frontend_check.py
@@ -0,0 +1,5 @@
+import time
+
+print("Running frontend checks...")
+time.sleep(3)
+print("Frontend checks passed.")
