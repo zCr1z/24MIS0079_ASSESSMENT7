320c7e96d2e5b887b83e21f7563a3dc3e3dd6bc8
Chris Thomas Binoj <91975002+zCr1z@users.noreply.github.com>
2026-09-17 04:07:39 +0000

    Edited file

--- project-2-archive-artifacts/app.py
+++ project-2-archive-artifacts/app.py
@@ -1,6 +1,6 @@
 with open("report.txt", "w", encoding="utf-8") as report:
     report.write("Application Report\n")
-    report.write("Total Users: 120\n")
-    report.write("Active Users: 45\n")
+    report.write("Total Users: 200\n")
+    report.write("Active Users: 67\n")
 
 print("Report generated.")
